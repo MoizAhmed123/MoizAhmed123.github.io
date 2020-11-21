0553e9889ff98c2723d52342671fe792a148df2a
ZSmaster1 <zaakirsheikh630@gmail.com>
2020-11-21 12:26:51 -0500

    updated login sys

--- login.php
+++ login.php
@@ -84,36 +84,17 @@
     </header>
 
     <!-- Main Content -->
-            <form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
 
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+    <form action="includes/login.inc.php" method="POST" class="form">
+      <input type="text" name="uid" placeholder="Username/Email..." class="form">
+      <input type="password" name="pwd" placeholder="Password..." class="form">
+      <button type="submit" name="submit" class="submitbutton">Log in</button class="submitbutton">
+    </form>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
-</div>
-</div>
-</div>
-</div>
-	
-	
-	  </ul>
+
+    
     <!-- Footer -->
 <!DOCTYPE HTML>
 <style type="text/css">
