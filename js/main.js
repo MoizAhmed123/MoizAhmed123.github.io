b59ac464a6f5a6b2e3ab76127974b274c1588d68
ZSmaster1 <zaakirsheikh630@gmail.com>
2020-11-21 13:35:43 -0500

    updated login sys

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    console.log("your username is " + username + "and your password is " + password)
+}
