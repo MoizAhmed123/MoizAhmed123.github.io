bd8ffe81d5cf0af90a5953a741d6f15944a47f8e
ZSmaster1 <zaakirsheikh630@gmail.com>
2020-11-21 13:54:30 -0500

    updated login sys

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-
+var objPeople = [
+    {
+        username: "aiq"
+        password: "test123"
+    },
+    {
+        username: "maaz"
+        password: "aiq"
+    }
+]
 
 function getInfo() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
-    console.log("your username is " + username + "and your password is " + password)
+
+    for(i = 0; i < objPeople.length; i++) {
+        if (username == objPeople[i].username && password == objPeople[i].password)  {
+            console.log(username + "is logged in")
+        }
+    }
 }
