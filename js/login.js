43898b388fcb6e8fdec9d40cc9ff7a3fa40787bd
ZSmaster1 <zaakirsheikh630@gmail.com>
2020-11-24 08:33:17 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,6 +4,11 @@ function validate()
     //get inputs from user
     const username=document.getElementById("username").value;
     const password=document.getElementById("password").value;
+    const form = document.getElementById("form").value;
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    })
 
     
     //validate inputs and redirect
