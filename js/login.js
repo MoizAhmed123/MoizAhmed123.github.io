201ead65c7ea177849d09d49d54aa140efe046a8
ZSmaster1 <zaakirsheikh630@gmail.com>
2020-11-24 08:29:32 -0500

    updates login sys

--- js/login.js
+++ js/login.js
@@ -2,15 +2,15 @@ function validate()
 {
 
     //get inputs from user
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
+
     
     //validate inputs and redirect
     if (username=="abbasalvi1"&& password=="test123") {
         window.location="https://assalaminstitute.ca/abbas_alvi.html";
         alert="login sucsessfull";
         return false;
-
     }
      
     
