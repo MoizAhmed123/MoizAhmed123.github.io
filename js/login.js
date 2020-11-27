dd9fbba36856a09a32c7228a097593ff730e3b00
Zayd Qadri <61556844+Zaydqadri@users.noreply.github.com>
2020-11-27 15:22:41 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,14 +10,3 @@ alert("Invalid Password");
 } else {  
 alert("Invalid Username");
 }
-} else {
-  if (form.identifier.value=="zaakir.sheikh") { 
-    console.log(form.identifier.value)
-    if (form.pass.value=="6302009") { 
-      window.location.assign('https://assalaminstitute.ca/zaakirS.html');
-    } else {
-    alert("Invalid Password");
-    } else {  
-    alert("Invalid Username");
-  }
-}
