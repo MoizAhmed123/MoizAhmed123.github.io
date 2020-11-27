ce41107daa88bdfdbcb9cbadff41adb0c3af5738
Zayd Qadri <61556844+Zaydqadri@users.noreply.github.com>
2020-11-27 12:02:01 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,29 +1,14 @@
-function validate() 
-{
+/*This Script allows people to enter by using a form that asks for a
+UserID and Password*/
+function pasuser(form) {
+if (form.identifier.value=="admin") { 
+console.log(form.identifier.value)
+if (form.pass.value=="user") { 
+  window.location.assign('https://assalaminstitute.ca/contact.html');
+} else {
+alert("Invalid Password");
+}
 
-    //get inputs from user
-    //var username = document.getElementById("username").value;
-    //var password = document.getElementById("password").value;
-
-    var url_string = (window.location.href).toLowerCase();
-    var url = new URL(url_string);
-    var username = url.SearchParams.get("username");
-    var password = url.SearchParams.get("password");
-
-    //validate inputs and redirect
-    if (username=="admin"&& password=="user") {
-        alert="login successful";
-      window.location.assign('https://assalaminstitute.ca/contact.html')
-    }
-    else {
-       alert("the username " + username + ", or the password " + password + ", was not correct"); 
-    }
-     
-    
-
-
-
-//failed login script
-//alert="incorrect username or password"
-//return false;
+} else {  alert("Invalid UserID");
+}
 }
